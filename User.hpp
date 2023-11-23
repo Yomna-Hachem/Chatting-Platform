119cf8e59efca31a2bd042a8034d5b53b2ab2cdc
Yomna-Hachem <126457557+Yomna-Hachem@users.noreply.github.com>
2023-11-23 23:46:58 +0200

    user class version 1

---
+++ User.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <vector> 
+#include <list>
+#include <algorithm>
+#include <cstring>
+#include <iterator>
+
+class User{
+
+private:
+
+    String name;
+    String username;
+    String Password;
+
+public:
+    void addChat(User u1);   
+
+}
