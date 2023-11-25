5693d3c0f5972038c69a273a65d7821acdceb64c
Yomna-Hachem <126457557+Yomna-Hachem@users.noreply.github.com>
2023-11-25 14:38:45 +0200

    ..

--- ChatManager.cpp
+++ ChatManager.cpp
@@ -3,15 +3,12 @@
 
     private:
 
-       Hmap<int , chat > map;
+      Hmap<int , chat > map;
 
-void createChat(string chatID, vector<User*> participants){
- //void createChat(string chatID, vector<int> participants) {
-        Chat* newChat = new Chat(chatID, participants);
-        insert(chatID ,newChat);
-        // int index = hash(chatID);
-        //map[index]= newChat;
-    }
+// void createChat(string chatID, vector<User*> participants){
+//         Chat* newChat = new Chat(chatID, participants);
+//         insert(chatID ,newChat);
+//     }
 
 
 
